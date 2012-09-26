d6a99d1cfe2a63cb050648e2b2a40002627ede52
Chris Radcliff <chris.radcliff@gmail.com>
2012-09-26 10:52:33 -0700

    Updated with Chris Woodall's newer version.

--- content.js
+++ content.js
@@ -1,71 +1,53 @@
-/*
-  Goverify - Social Media Verification
-    Christopher Woodall
-    woodall.christopher@gmail.com
-    http://www.christopherwoodall.com
-*/
-'use strict';
-var d,usasmr,Request,Goverify;
+var usasmr = "http://registry.usa.gov/accounts/verify.json?" +
+         "service_url=" + window.location.href;
 
-// Variables
-d = document;
-usasmr = "http://registry.usa.gov/accounts/verify.json?" +
-         "service_url=" + window.location.href;  /* USA.gov Social Media
-                                                    Registry API
-                                                */
+var DOM = {
+  insert: function( strElement, strData, strId )
+  {
+    objElement = document.createElement(strElement);
+    if( strId ){ objElement.setAttribute('id', strId); }
+    objElement.innerHTML = strData;
+    document.body.appendChild(objElement);
+  },
+  update: function( strId, strData )
+  {
+    objId = document.getElementById(strId);
+    objId.innerHTML = strData;
+  },
+  destroy: function( strID )
+  {
+    return (elem=document.getElementById(strID)).parentNode.removeChild(elem); 
+  }
+}
 
-function Goverify( data )
-{
-  data = JSON.parse(data);
-  if ( !data || data.length<1 ) return;
-  if ( !data.verified ) return;
-  //append my css file.. tracking? 
-  document.getElementsByTagName("body")[0].innerHTML += [
-"<link rel=\"stylesheet\" href=\"//christopherwoodall.com/goverify/css/foundation.css\">",
-"<div class=\"alert-box alert\" style=\"position: fixed;top: 0;left: 0;z-index: 9999;width: 100%;\">",
+var Goverify = {
+  template: '<div class="goverify_addon_chrome"><div class="close" onclick="this.parentNode.style.display=\'none\'">close</div><div class="goverify_content_chrome">Goverify<div id="goverify_addon_chrome_results"></div><hr \></div></div>',
+  init: function( data ) {
+    data = JSON.parse(data);
+    if ( !data || data.length<1 ) return;
+    if ( !data.verified ) return;
 
-"  <div class=\"row\">",
-"    <div class=\"twelve columns\">",
-"      <div class=\"three columns\">",
-"        <h2>Goverify</h2>",
+	results = '<p>This page belongs to ' +
+	          data.agencies[0].agency_name +
+			  ' an organization of the ' + 
+			  data.organization +
+	          '.</p><p>Updated ' +
+			  data.updated_at +
+			  ' by ' +
+			  data.updated_by +
+			  '</p><p><a href="' +
+			  data.details_url +
+			  ' \>More</a>';
+			  
+	DOM.insert("div", Goverify.template, 'klhh9867');
+	DOM.update('goverify_addon_chrome_results', results);
 
-"        <p>Government Verification</p>",
-"      </div>",
-"      <div class=\"six columns\">		<!-- Content -->",
-"        <div class=\"row\">",
-
-"          <div class=\"twelve columns\" style=\"padding-top:24px;color:#000;\">",
-"	    WATCH ON YOUTUBE belongs to the "+data.agencies[0].agency_name+", an organization of the "+data.organization+".",
-"	  </div>",
-"	</div>",
-"	<div class=\"row\">",
-"	  <div class=\"twelve columns\" style=\"padding-top:6px;padding-bottom:6px;\">",
-"	    <span class=\"radius secondary label\">",
-"	      <a href=\"http://registry.usa.gov"+data.details_url+"\">More</a>",
-"	    </span>",
-"	  </div>",
-"        </div>",
-"	<div class=\"row\">",
-"	  <div class=\"twelve columns\" style=\"color:#000\">",
-"            Updated "+data.updated_at+" by "+data.updated_by,
-"          </div>",
-"	</div>",
-"      </div>",
-"      <div class=\"three columns\">",
-"        <iframe src=\"http://rcm.amazon.com/e/cm?t=myfaonats-20&o=1&p=6&l=bn1&mode=pc-hardware&browse=13900871&fc1=000000&lt1=_blank&lc1=3366FF&bg1=FFFFFF&f=ifr\" marginwidth=\"0\" marginheight=\"0\" width=\"120\" height=\"150\" border=\"0\" frameborder=\"0\" style=\"border:none;\" scrolling=\"no\"></iframe>",
-"      </div>",
-"      <a href=\"\" class=\"close\">",
-"        <span class=\"has-tip\" title=\"Close\">&times;</span>",
-"      </a>",
-"    </div>",
-"  </div>		<!-- Content -->",
-"</div>			<!-- /Master View -->",].join('\n');
-  /*document.write();*/
-  return;
+    return;
+  }
 }
 
 /* General Functions */
-Request = function( url )
+var Request = function( url )
 {
   this.url = url;
   this.callback = console.log; //default
@@ -89,6 +71,6 @@ Request.prototype.get = function()
 }
 
 /* Launcher */
-var a = new Request(usasmr);
-a.callback = Goverify;
-a.get();
+var main = new Request(usasmr);
+main.callback = Goverify.init;
+main.get();
